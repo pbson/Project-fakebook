6ac2b8948cfab88fed9769696a4031ac4f674be4
Le Xuan Quang <Quang.lxun@gmail.com>
2020-10-17 21:31:44 +0700

    a

--- controller/chatController.js
+++ controller/chatController.js
@@ -1,38 +1,22 @@
 const Message = require("../models/Message");
-
+const botName = 'ChatCord Bot';
 module.exports = (io) => {
-    io.on("connection", async (client) => {
-        console.log("new connection"+ client.id);
-        // client.emit("joinchat",
-        //     messageAttributes = {
-        //         Content: data.Content,
-        //         Sender: data.Sender,
-        //     }        
-        // )
-        client.on("disconnect", () => {
-            console.log("user disconnected");
-        });
-        //Listen to event message
-        client.on("message",async data => {
-            let messageAttributes = {
-                Content: data.Content,
-                Sender: data.Sender,
-            };
-            //save sent message to database
-            try {
-                m = new Message(messageAttributes);
-                await m.save();
-            } catch (error) {
-                console.log(error);
-            }
-            //forward message to users
-            io.emit("message", 
-                messageAttributes
-            );
-        });
-
-        //GET most recent messages
-        let currentMessage = await Message.find({}).sort({ createdAt: -1 }).limit(10)
-        client.emit("load all messages", currentMessage.reverse())
+    io.on("connection", async (socket) => {
+        socket.on('joinChat', ({ userid, conversation }) => {
+            socket.join(conversation);
+            console.log(socket.id)
+            io.to(conversation).emit('roomUsers', {
+              room: conversation,
+              users: userid
+            });
+          });
+          socket.on('chatMessage', data => {
+            message = {}
+            message.username = data.userid
+            message.text = data.message
+            message.time = "aaaa"
+            io.to(data.conversation).emit('message',message);
+          });
     });
+
 };
